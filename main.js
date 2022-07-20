1dc434502da93fd5c132bd2fd28b79075f720803
davide <dacapoccia@gmail.com>
2022-07-20 16:43:35 +0200

    ok

--- main.js
+++ main.js
@@ -1,14 +1,19 @@
 let startBtn = document.getElementById('start');
 let quadGrid = document.getElementById('grid');
 let clicked = document.getElementById('level');
+let scoredPoints = document.getElementById('score');
+let bombs = [];
 
 
 startBtn.addEventListener('click',
     function(){
         quadGrid.innerHTML= '';
+        score.innerHTML='';
         createGrid(clicked.value);
+        started = true;
+        count = 1;
     }
-)
+);
 
 
 
@@ -31,15 +36,39 @@ function createGrid(level){
         break;
     }
 
+    bombs = [];
+
+    for (let y=1; y<=16; y++){
+        const createBombs = createRandomNumber(bombs, 1, sqrNumber);
+        bombs.push(createBombs);
+        
+    }
+    console.log(bombs);
+
     for(x=1; x<=sqrNumber; x++){
         let mySqr = createSqr();
         mySqr.append(x);
         mySqr.classList.add(sqrPlus);
         quadGrid.append(mySqr);
         clickedStyle(mySqr);
+        // selectedAlts(mySqr);
     }
 }
 
+let count = 0;
+let started = true;
+
+
+function createRandomNumber(clickedSqrs, min, max){
+    let cleanSqr = false;
+    let randomNumber;
+    while (!cleanSqr){
+        randomNumber = Math.floor(Math.random()*(max - min)+1 - min);
+        if(clickedSqrs.includes(randomNumber)==false){
+            cleanSqr =true;
+        }
+    } return randomNumber;
+}
 
 function createSqr(){
     let square = document.createElement('div');
@@ -51,9 +80,19 @@ function createSqr(){
 function clickedStyle(mySqr){
     mySqr.addEventListener('click',
         function(){
-            this.classList.toggle('selected1');
+            // this.classList.toggle('selected1');
         
             console.log(mySqr.innerHTML);
+            if(started){
+                if (bombs.includes(parseInt(mySqr.innerHTML))){
+                    this.classList.add('selectedBomb');
+                    started = false;
+                } else{
+                    console.log(this, this.classList);
+                    this.classList.add('selected1');
+                }
+            }  score.innerHTML = `<p>punti totalizzati: ${count++}</p>`;  
         }
     )
-}
+};
+
